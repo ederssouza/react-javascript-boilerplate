65288716f8382181a90b401fb12e076aa570cd22
Eder Sampaio <edersampaio@outlook.com.br>
2022-09-15 12:21:50 -0300

    feat: add react testing library and config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   testPathIgnorePatterns: ['/dist/', '/node_modules/', '/public/'],
   collectCoverageFrom: ['src/**/*.js(x)?', '!src/**/index.js', '!src/main.jsx'],
   modulePaths: ['<rootDir>/src/'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
   }
